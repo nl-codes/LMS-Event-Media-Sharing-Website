86be6b4d4acb31d47ee35c6abc66979d8ac6f9d6
lhn_nryn <narayanlohani07@gmail.com>
2025-11-11 22:35:04 +0545

    Setup: Token (JWT) Generation

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,10 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (payload) => {
+    const secret = process.env.JWT_SECRET_KEY;
+    if (!secret) {
+        throw new Error("JWT secret key is missing in environment variables");
+    }
+
+    return jwt.sign(payload, secret, { expiresIn: "1h" });
+};
