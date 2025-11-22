f275ae95e12515ff9dd2125fc5450217517291c5
lhn_nryn <narayanlohani07@gmail.com>
2025-11-22 20:15:50 +0545

    Setup: Frontend directory init

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { jetBrainsMono } from "./styles/fonts";
+import "@/styles/globals.css";
+
+export const metadata: Metadata = {
+    title: "LMS 24: Live Media Sharing 24 hours",
+    description:
+        "Real-time event media sharing made easy. Create groups, scan a QR code, and let participants upload and view photos instantly",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={`${jetBrainsMono.className} antialiased`}>
+                {children}
+            </body>
+        </html>
+    );
+}
