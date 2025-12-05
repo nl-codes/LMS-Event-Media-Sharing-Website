f60a463946f3ecb0bdc3c5608c1cf5ce1c98072e
lhn_nryn <dubmate2244@gmail.com>
2025-12-05 19:50:07 +0545

    Refactor: Change landing screen picture

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import HomeButtonsGroup from "./HomeButtonsGroup";
 
 export default function Home() {
     return (
-        <div className="flex mt-12 justify-around">
+        <div className="flex mt-[100] justify-around">
             <div className="flex flex-col items-center justify-center gap-8 w-[400px]">
                 <p className="text-6xl font-extrabold text-left">
                     Live Media Sharing
@@ -16,9 +16,10 @@ export default function Home() {
             </div>
 
             <Image
-                src="https://res.cloudinary.com/dimgh55x6/image/upload/v1763879563/lms_landing_b7oxfx.png"
-                width="700"
-                height="500"
+                className="rounded-2xl"
+                src="/bg-landing.png"
+                width="900"
+                height="700"
                 alt="Global Connection"
             />
         </div>
