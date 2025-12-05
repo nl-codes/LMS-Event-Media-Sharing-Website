9ed48fe9fbe4d263ccf5bba85b9edc2c4afcf2a8
lhn_nryn <dubmate2244@gmail.com>
2025-12-05 20:16:57 +0545

    Refactor: Remove picture from sign up screen

--- frontend/src/app/(auth)/signup/page.tsx
+++ frontend/src/app/(auth)/signup/page.tsx
@@ -1,16 +1,9 @@
 import LoginExistingButton from "./LoginExistingButton";
 import SignupForm from "./signup-form";
-import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="mt-40 flex justify-around items-start">
-            <Image
-                src="https://res.cloudinary.com/dimgh55x6/image/upload/v1763879563/lms_landing_b7oxfx.png"
-                width="360"
-                height="255"
-                alt="Global Connection"
-            />
+        <div className="mt-40 flex gap-[100] justify-center">
             <div className="flex flex-col w-[360px] gap-5">
                 <p className="font-extrabold text-4xl">Sign up</p>
                 <p className="font-medium text-xl">
