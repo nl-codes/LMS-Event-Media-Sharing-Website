dc740b834d5968906a8d30631b72939efa8123a3
lhn_nryn <dubmate2244@gmail.com>
2025-12-12 21:39:59 +0545

    Re-activation fields added

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,13 +5,23 @@ const userSchema = new Schema(
         userName: { type: String, required: true, trim: true },
         email: { type: String, required: true, trim: true, unique: true },
         password: { type: String, required: true, trim: true },
+
         status: {
             type: String,
             enum: ["pending", "active", "suspended"],
             default: "pending",
         },
+
         activationToken: String,
         activationExpires: Date,
+
+        activationResendCount: {
+            type: Number,
+            default: 0,
+        },
+        activationResendLastSentAt: {
+            type: Date,
+        },
     },
     { timestamps: true }
 );
