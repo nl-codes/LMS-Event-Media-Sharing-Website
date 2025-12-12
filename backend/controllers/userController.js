3028b3941da11b5f888086fc2b1803605f3f54d9
lhn_nryn <dubmate2244@gmail.com>
2025-12-12 19:56:35 +0545

    userController: sendEmail after user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,17 +1,34 @@
-import { addUsers, verifyUser } from "../services/userService.js";
-import { generateToken } from "../utils/generateToken.js";
+import {
+    addUsers,
+    verifyUser,
+    verifyUserActivationToken,
+} from "../services/userService.js";
+import { generateActivationToken } from "../utils/activation/generateActivationToken.js";
+import { generateToken } from "../utils/auth/generateToken.js";
+import sendEmail from "../utils/sendEmail.js";
 
 export const registerUser = async (req, res) => {
     try {
         const registeredUser = await addUsers(req.body);
         // console.log(registeredUser);
-        const token = generateToken({
-            email: registeredUser.email,
-            userName: registeredUser.userName,
-        });
-        res.status(200).json({
-            message: "Registration Successfull",
-            token: token,
+        const { token, tokenHash, expires } = generateActivationToken();
+        registeredUser.activationToken = tokenHash;
+        registeredUser.activationExpires = expires;
+        registeredUser.status = "pending";
+
+        await registeredUser.save();
+
+        const activationUrl = `${process.env.BACKEND_URL}/users/activate?token=${token}`;
+
+        await sendEmail(
+            registeredUser.email,
+            "Activate your account",
+            `Your activation link: ${activationUrl}`,
+            `<div style="font-family: Arial, sans-serif; padding: 20px; background: #f5f5f5;"><div style="max-width: 500px; margin: auto; background: #ffffff; padding: 20px; border-radius: 8px;"><h2 style="text-align: center; color: #333;">Welcome to Our Live Media Sharing 🎉</h2><p style="font-size: 16px; color: #444;">You're almost there! Click the button below to activate your account.</p><div style="text-align: center; margin: 30px 0;"><a href="${activationUrl}"style="background: #4CAF50; color: white; padding: 12px 20px;text-decoration: none; border-radius: 5px; font-weight: bold;">Activate Account</a></div><p style="font-size: 14px; color: #666;">Or copy and paste this link into your browser:</p><p style="word-break: break-all; color: #007BFF;">${activationUrl}</p><hr style="margin: 20px 0; border: none; border-top: 1px solid #ddd;" /><p style="font-size: 12px; color: #888; text-align: center;">If you did not create this account, you can safely ignore this email.</p></div></div>`
+        );
+
+        res.status(201).json({
+            message: "Signup successful. Check email to activate account.",
         });
     } catch (err) {
         let status = 400;
