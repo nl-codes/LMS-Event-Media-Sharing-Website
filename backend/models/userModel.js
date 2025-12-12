d8b8a5ec30428ca2d2d07039e141be1ff146d719
lhn_nryn <dubmate2244@gmail.com>
2025-12-12 19:02:19 +0545

    userModel: Add account status

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,9 +1,19 @@
 import { model, Schema } from "mongoose";
 
-const userSchema = new Schema({
-    userName: { type: String, required: true, trim: true },
-    email: { type: String, required: true, trim: true },
-    password: { type: String, required: true, trim: true },
-});
+const userSchema = new Schema(
+    {
+        userName: { type: String, required: true, trim: true },
+        email: { type: String, required: true, trim: true, unique: true },
+        password: { type: String, required: true, trim: true },
+        status: {
+            type: String,
+            enum: ["pending", "active", "suspended"],
+            default: "pending",
+        },
+        activationToken: String,
+        activationExpires: Date,
+    },
+    { timestamps: true }
+);
 
 export const User = model("User", userSchema);
