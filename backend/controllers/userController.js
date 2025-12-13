3d0276b56ea782332824592a3296e60a81fc16d0
lhn_nryn <dubmate2244@gmail.com>
2025-12-13 11:55:00 +0545

    Chore: usage of renamed generateJWTtoken

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,7 @@ import {
     verifyUserActivationToken,
 } from "../services/userService.js";
 import { generateGeneralToken } from "../utils/generateToken.js";
-import { generateToken } from "../utils/auth/generateToken.js";
+import { generateJWTtoken } from "../utils/auth/generateJWTtoken.js";
 import {
     getActivationEmailHTML,
     getReactivationEmailHTML,
@@ -48,7 +48,7 @@ export const registerUser = async (req, res) => {
 export const loginUser = async (req, res) => {
     try {
         const loginUser = await verifyUser(req.body);
-        const token = generateToken({
+        const token = generateJWTtoken({
             email: loginUser.email,
             userName: loginUser.userName,
         });
