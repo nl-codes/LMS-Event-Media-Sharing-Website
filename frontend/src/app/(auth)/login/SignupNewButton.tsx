d457a35ffbcc7f53882e7334c029f6f4ac58398e
lhn_nryn <dubmate2244@gmail.com>
2025-12-13 18:43:36 +0545

    Add button to navigate to forgot-password

--- frontend/src/app/(auth)/login/SignupNewButton.tsx
+++ frontend/src/app/(auth)/login/SignupNewButton.tsx
@@ -4,14 +4,24 @@ import { useRouter } from "next/navigation";
 export default function SignupNewButton() {
     const router = useRouter();
     const handleSignup = () => router.replace("/signup");
+    const handleForgotPassword = () => router.replace("/forgot-password");
     return (
-        <p>
-            Don&apos;t have an account?{" "}
-            <button
-                className="text-blue-600 hover:underline hover:cursor-pointer"
-                onClick={handleSignup}>
-                Sign up
-            </button>
-        </p>
+        <>
+            <p>
+                Don&apos;t have an account?{" "}
+                <button
+                    className="text-blue-600 hover:underline hover:cursor-pointer"
+                    onClick={handleSignup}>
+                    Sign up
+                </button>
+            </p>
+            <p>
+                <button
+                    className="text-blue-600 hover:underline hover:cursor-pointer"
+                    onClick={handleForgotPassword}>
+                    Forgot Password?
+                </button>
+            </p>
+        </>
     );
 }
