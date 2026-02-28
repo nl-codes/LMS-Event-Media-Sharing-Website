a14f7a96b9ad61fe0022bd77208ac89d7dd916c9
lhn_nryn <dubmate2244@gmail.com>
2026-02-28 12:31:11 +0545

    Pass userName through SignedHeader

---
+++ frontend/src/app/home/layout.tsx
@@ -0,0 +1,28 @@
+import SignedHeader from "@/components/headers/SignedHeader";
+import { userJWTToken } from "@/types/User";
+import { jwtDecode } from "jwt-decode";
+import { cookies } from "next/headers";
+
+export default async function UserLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    let userName = "";
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token")?.value;
+    if (token) {
+        try {
+            const decoded = jwtDecode<userJWTToken>(token);
+            userName = decoded.userName || "";
+        } catch {
+            // ignore decode errors
+        }
+    }
+    return (
+        <>
+            <SignedHeader userName={userName} />
+            {children}
+        </>
+    );
+}
