0902ebc110228e744982dc9991bae790cf7bb6f2
lhn_nryn <dubmate2244@gmail.com>
2026-03-01 17:48:46 +0545

    add controller to handle edit profile

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -1,5 +1,9 @@
 import { User } from "../models/userModel.js";
-import { createProfile, getProfile } from "../services/profileService.js";
+import {
+    createProfile,
+    getProfile,
+    updateProfile,
+} from "../services/profileService.js";
 
 export const addProfile = async (req, res) => {
     try {
@@ -35,6 +39,30 @@ export const retrieveProfile = async (req, res) => {
         res.status(200).json({ profile });
     } catch (err) {
         console.error("❌ Error retrieving profile: ", err);
+        const status = err.message === "Profile not found" ? 404 : 400;
+        res.status(status).json({ error: err.message });
+    }
+};
+
+export const editProfile = async (req, res) => {
+    try {
+        const { email } = req.user;
+        const user = await User.findOne({ email });
+        if (!user) throw new Error("User not found");
+
+        const newImageUrl = req.file ? req.file.path : null;
+
+        const updatedProfile = await updateProfile(
+            user._id,
+            req.body,
+            newImageUrl,
+        );
+        res.status(200).json({
+            message: "Profile updated successfully",
+            profile: updatedProfile,
+        });
+    } catch (err) {
+        console.error("❌ Error updating profile: ", err);
         const status = err.message === "Profile not found" ? 404 : 400;
         res.status(status).json({ error: err.message });
     }
