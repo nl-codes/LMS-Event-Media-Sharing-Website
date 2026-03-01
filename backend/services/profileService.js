fae6b2ded8f52a83f2a5987e2162150cf612d615
lhn_nryn <dubmate2244@gmail.com>
2026-03-01 10:46:47 +0545

    Create Profile service

---
+++ backend/services/profileService.js
@@ -0,0 +1,23 @@
+import { User } from "../models/userModel.js";
+import { Profile } from "../models/profileModel.js";
+
+export const createProfile = async (userId, profileData) => {
+    const user = await User.findById(userId);
+    if (!user) throw new Error("User not found");
+
+    const existingProfile = await Profile.findOne({ user: userId });
+    if (existingProfile)
+        throw new Error("Profile already exists for this user");
+
+    const { firstName, lastName, bio, profilePicture } = profileData;
+
+    const newProfile = new Profile({
+        user: userId,
+        firstName,
+        lastName,
+        bio,
+        profilePicture: profilePicture || "",
+    });
+
+    return await newProfile.save();
+};
