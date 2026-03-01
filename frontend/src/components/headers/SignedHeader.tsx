8ecdb53f24c2654cdf8f59e91c592b7f1034d1e2
lhn_nryn <dubmate2244@gmail.com>
2026-03-01 19:21:02 +0545

    Use link to navigate to home in new logo component

--- frontend/src/components/headers/SignedHeader.tsx
+++ frontend/src/components/headers/SignedHeader.tsx
@@ -1,17 +1,17 @@
 "use client";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import Button from "../buttons/Button";
 import { FaRegUser } from "react-icons/fa";
 import { FiLogOut } from "react-icons/fi";
 import { backend_url } from "@/config/backend";
 import { useUser } from "@/context/UserContext";
+import LogoRounded80 from "../logo/Logo_Rounded_80";
+import Link from "next/link";
 
 export default function SignedHeader({ userName }: { userName: string }) {
     const router = useRouter();
     const { setUser } = useUser();
 
-    const handleLogoClick = () => router.replace("/home");
     const handleProfileClick = () => router.push("/home/profile");
 
     const logout = async () => {
@@ -31,15 +31,9 @@ export default function SignedHeader({ userName }: { userName: string }) {
         <header className="w-full pt-4">
             <div className="flex items-center justify-between px-16 pb-4 border-b-2 border-cusblue">
                 {/* Logo Container */}
-                <div className="cursor-pointer" onClick={handleLogoClick}>
-                    <Image
-                        src="https://res.cloudinary.com/dimgh55x6/image/upload/v1763878601/lms_logo_fw6m2q.png"
-                        alt="Logo"
-                        width={80}
-                        height={80}
-                        className="object-contain"
-                    />
-                </div>
+                <Link href="/home" replace>
+                    <LogoRounded80 />
+                </Link>
 
                 {/* Navigation Actions */}
                 <div className="flex items-center gap-4">
