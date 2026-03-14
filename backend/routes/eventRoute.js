2b9923c0a97b03751879a3250cb5ccad9118185c
lhn_nryn <dubmate2244@gmail.com>
2026-03-14 12:10:55 +0545

    add get event by Id route

--- backend/routes/eventRoute.js
+++ backend/routes/eventRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { requireAuth } from "../middleware/authMiddleware.js";
-import { registerEvent } from "../controllers/eventController.js";
+import { registerEvent, getEventById } from "../controllers/eventController.js";
 
 const router = express.Router();
 // Protected routes (require authentication)
 router.post("/", requireAuth, registerEvent);
+router.get("/details/:id", requireAuth, getEventById);
 
 export default router;
