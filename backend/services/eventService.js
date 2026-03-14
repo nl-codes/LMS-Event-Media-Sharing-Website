eaed46cc3023b13fc28f250ef92725cc8aa81da1
lhn_nryn <dubmate2244@gmail.com>
2026-03-14 10:39:53 +0545

    fetch users id from db and pass to finalEventData

--- backend/services/eventService.js
+++ backend/services/eventService.js
@@ -1,12 +1,21 @@
-import Event from "../models/eventModel.js";
+import { Event } from "../models/eventModel.js";
+import { User } from "../models/userModel.js";
 
 export const createEvent = async (eventData) => {
     try {
         if (new Date(eventData.startTime) >= new Date(eventData.endTime)) {
             throw new Error("Start time must be before end time");
         }
+        const user = await User.findOne({ email: eventData.hostEmail }).select(
+            "_id",
+        );
 
-        const event = new Event(eventData);
+        const finalEventData = {
+            ...eventData,
+            hostId: user._id,
+        };
+
+        const event = new Event(finalEventData);
         await event.save();
 
         // Populate host information
