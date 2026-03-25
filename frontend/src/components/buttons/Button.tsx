2f681f85202fb6692d185bc94d25315e88fda8e7
lhn_nryn <dubmate2244@gmail.com>
2026-03-25 15:40:36 +0545

    Add disable property to button

--- frontend/src/components/buttons/Button.tsx
+++ frontend/src/components/buttons/Button.tsx
@@ -9,18 +9,21 @@ export default function Button({
     handleClick,
     type = "button",
     loading = false,
+    disabled,
+    ...props
 }: ButtonProps) {
     return (
         <button
             type={type}
             onClick={handleClick}
-            disabled={loading}
+            disabled={loading || disabled}
             className={clsx(
                 "flex justify-center items-center gap-4 bg-cusblue text-cuscream p-2 rounded-lg font-bold transition-opacity",
                 "hover:bg-cusviolet",
-                loading && "opacity-50 cursor-not-allowed",
-                className
-            )}>
+                (loading || disabled) && "opacity-50 cursor-not-allowed",
+                className,
+            )}
+            {...props}>
             {loading ? "Loading..." : children}
         </button>
     );
