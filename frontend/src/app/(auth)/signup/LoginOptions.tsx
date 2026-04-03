35a67083c148f1269c15453b53185f614d45ab32
lhn_nryn <dubmate2244@gmail.com>
2026-04-03 08:51:20 +0545

    reanme loginNewButto to LoginOptions

--- frontend/src/app/(auth)/signup/LoginOptions.tsx
+++ frontend/src/app/(auth)/signup/LoginOptions.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useRouter } from "next/navigation";
 
-export default function LoginExistingButton() {
+export default function LoginOptions() {
     const router = useRouter();
     const handleLogin = () => router.replace("/login");
     return (
