fe9d99a3473966cdc100d8f7078bc421a5e9d4e8
lhn_nryn <dubmate2244@gmail.com>
2026-04-03 07:33:00 +0545

    Add icons to login and signup buttons

--- frontend/src/components/LandingPageButtonGroup.tsx
+++ frontend/src/components/LandingPageButtonGroup.tsx
@@ -1,23 +1,27 @@
 "use client";
-import LandingButton from "@/components/buttons/LandingButton";
+
+import Button from "@/components/buttons/Button";
 import { useRouter } from "next/navigation";
+import { ArrowRight, UserPlus } from "lucide-react";
 
 export default function LandingPageButtonGroup() {
     const router = useRouter();
+
     return (
-        <div className="button-area flex gap-8 justify-center items-center">
-            <LandingButton
-                handleClick={() => {
-                    router.push("/login");
-                }}>
-                Login
-            </LandingButton>
-            <LandingButton
-                handleClick={() => {
-                    router.push("/signup");
-                }}>
-                Signup
-            </LandingButton>
+        <div className="button-area flex flex-col sm:flex-row gap-4 sm:gap-6 justify-center items-center">
+            <Button
+                handleClick={() => router.push("/login")}
+                className="w-full sm:w-48 shadow-xl">
+                <span>Login</span>
+                <ArrowRight className="w-4 h-4" />
+            </Button>
+
+            <Button
+                handleClick={() => router.push("/signup")}
+                className="w-full sm:w-48 shadow-xl">
+                <span>Sign up</span>
+                <UserPlus className="w-4 h-4" />
+            </Button>
         </div>
     );
 }
