51df49d0af8fe467ce4cfe537f1fc263fb3cce4f
lhn_nryn <dubmate2244@gmail.com>
2026-04-03 07:48:51 +0545

    use generic logo component

--- frontend/src/components/headers/SignedHeader.tsx
+++ frontend/src/components/headers/SignedHeader.tsx
@@ -5,8 +5,8 @@ import { FaRegUser } from "react-icons/fa";
 import { FiLogOut } from "react-icons/fi";
 import { backend_url } from "@/config/backend";
 import { useUser } from "@/context/UserContext";
-import LogoRounded80 from "../logo/Logo_Rounded_80";
 import Link from "next/link";
+import LogoRounded from "../logo/Logo_Rounded";
 
 export default function SignedHeader({ userName }: { userName: string }) {
     const router = useRouter();
@@ -32,7 +32,7 @@ export default function SignedHeader({ userName }: { userName: string }) {
             <div className="flex items-center justify-between px-16 pb-4 border-b-2 border-cusblue">
                 {/* Logo Container */}
                 <Link href="/home" replace>
-                    <LogoRounded80 />
+                    <LogoRounded size={80} />
                 </Link>
 
                 {/* Navigation Actions */}
