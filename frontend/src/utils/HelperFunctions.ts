45ef7a2715c1259925a4f0901d34ed55d80c1427
lhn_nryn <dubmate2244@gmail.com>
2026-04-04 22:04:26 +0545

    Add function triggerBrowserDownload

--- frontend/src/utils/HelperFunctions.ts
+++ frontend/src/utils/HelperFunctions.ts
@@ -48,3 +48,15 @@ export const handleCopyText = async (textToCopy: string) => {
         toast.error("Failed to copy link");
     }
 };
+
+export const triggerBrowserDownload = (blob: Blob, filename: string) => {
+    const objectUrl = URL.createObjectURL(blob);
+    const anchor = document.createElement("a");
+    anchor.href = objectUrl;
+    anchor.download = filename;
+    anchor.style.display = "none";
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+    URL.revokeObjectURL(objectUrl);
+};
