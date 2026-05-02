73aac0c63ee9f05968c729dd980e4d238f8f688f
lhn_nryn <dubmate2244@gmail.com>
2026-05-02 10:43:59 +0545

    function to get list of admins

--- backend/services/superAdminService.js
+++ backend/services/superAdminService.js
@@ -18,3 +18,18 @@ export const approveAdminUser = async (adminId) => {
 
     return await safeUserForAdmin(approvedAdmin);
 };
+
+export const getAdminsList = async (searchTerm) => {
+    const q = { role: "admin" };
+    if (searchTerm) {
+        q.$or = [
+            { email: { $regex: searchTerm, $options: "i" } },
+            { userName: { $regex: searchTerm, $options: "i" } },
+        ];
+    }
+    const filteredAdmin = await User.find(q)
+        .sort({ createdAt: -1 })
+        .limit(500)
+        .select("_id userName email role status createdAt");
+    return filteredAdmin;
+};
