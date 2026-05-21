f69187a012eacbb7e3de6abf443b553ede18ee1f
lhn_nryn <dubmate2244@gmail.com>
2026-05-22 03:08:55 +0545

    add post route for /event-checkout and /event-checkout/confirm

--- backend/routes/paymentRoute.js
+++ backend/routes/paymentRoute.js
@@ -1,13 +1,26 @@
 import express from "express";
 import { requireAuth } from "../middleware/authMiddleware.js";
+import { attachEventId } from "../middleware/utilsMiddleware.js";
+import { uploadEventThumbnail } from "../middleware/uploadMiddleware.js";
 import {
     confirmCheckoutSession,
+    confirmEventCreateCheckout,
     createCheckoutSession,
+    startEventCreateCheckout,
 } from "../controllers/paymentController.js";
 
 const router = express.Router();
 
 router.post("/checkout", requireAuth, createCheckoutSession);
 router.post("/confirm", requireAuth, confirmCheckoutSession);
+
+router.post(
+    "/event-checkout",
+    requireAuth,
+    attachEventId,
+    uploadEventThumbnail.single("thumbnail"),
+    startEventCreateCheckout,
+);
+router.post("/event-checkout/confirm", requireAuth, confirmEventCreateCheckout);
 
 export default router;
