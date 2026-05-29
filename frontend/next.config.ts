cd1840cb82586015201235679fedb58557b6f54d
lhn_nryn <dubmate2244@gmail.com>
2026-05-29 21:02:41 +0545

    Update Cloudinary URL in next.config.ts to use environment variable

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
+const cloudinaryCloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
+
 const nextConfig: NextConfig = {
     images: {
         remotePatterns: [
             new URL(
-                "https://res.cloudinary.com/lhn-nryn-image-cloud/image/upload/**",
+                `https://res.cloudinary.com/${cloudinaryCloudName}/image/upload/**`,
             ),
         ],
     },
