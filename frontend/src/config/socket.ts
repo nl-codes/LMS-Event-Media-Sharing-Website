fc50afc91c3b5009cd60cd029a750dd551e0845b
lhn_nryn <dubmate2244@gmail.com>
2026-05-31 00:33:28 +0545

    Update socket connection URL to use production endpoint

--- frontend/src/config/socket.ts
+++ frontend/src/config/socket.ts
@@ -5,11 +5,14 @@ let socket: Socket;
 
 export const getSocket = (): Socket => {
     if (!socket) {
-        socket = io(backend_url || "http://localhost:3000", {
-            transports: ["websocket"],
-            autoConnect: false,
-            withCredentials: true,
-        });
+        socket = io(
+            backend_url ||
+                "https://lms-event-media-sharing-website.onrender.com",
+            {
+                autoConnect: false,
+                withCredentials: true,
+            },
+        );
     }
 
     return socket;
